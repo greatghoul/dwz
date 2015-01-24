8904f20afc5e03cceaeb9e8403ecb6936997b79f
greatghoul <greatghoul@gmail.com>
2015-01-25 05:27:33 +0800

    实现基础的缩短网址的功能

---
+++ js/application.js
@@ -0,0 +1,51 @@
+(function() {
+  function getParameterByName(name) {
+    var match = RegExp('[?&]' + name + '=([^&]*)').exec(window.location.search);
+    return match && decodeURIComponent(match[1].replace(/\+/g, ' '));
+  }
+
+  
+    
+  $(function() {
+    var $url_long   = $('#url-long')
+      , $url_short  = $('#url-short')
+      , $btn_submit = $('#btn-submit');
+
+    function shorterUrl(url) {
+      var url = $url_long.val();
+      
+      $url_short.val('');
+      $url_short.parent().removeClass('has-error');
+
+      if (!url) return;
+
+      $.ajax({
+        url: 'http://hget.sinaapp.com/post/http://dwz.cn/create.php',
+        data: { url: url },
+        dataType: 'json',
+        beforeSend: function() {
+          $btn_submit.attr('disabled', 'disabled');
+        },
+        success: function(result) {
+          if (result.err_msg) {
+            $url_short.val(result.err_msg);
+            $url_short.parent().addClass('has-error');
+          } else {
+            $url_short.val(result.tinyurl).focus().select();
+          }
+        },
+        error: function() {
+          $url_short.val('请求失败，请重试。');
+          $url_short.parent().addClass('has-error');
+        },
+        complete: function() {
+          $btn_submit.removeAttr('disabled');    
+        }
+      });
+    }
+
+    $url_long.val(getParameterByName('url'));
+    $btn_submit.on('click', shorterUrl)
+    $btn_submit.trigger('click');
+  });  
+})();
