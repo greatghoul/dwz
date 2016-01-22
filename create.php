4675e5ee6affa66ba166ae7d7b54e4294424e661
greatghoul <greatghoul@gmail.com>
2016-01-22 13:21:15 +0800

    create 同时支持 GET 和 POST 请求
    移除不再使用的 bootstrap.css 文件

--- create.php
+++ create.php
@@ -4,7 +4,7 @@ $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, "http://dwz.cn/create.php");
 curl_setopt($ch, CURLOPT_POST, true);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_POSTFIELDS, $_POST);
+curl_setopt($ch, CURLOPT_POSTFIELDS, array_merge($_GET, $_POST));
 
 $data = curl_exec($ch);
 curl_close($ch);
